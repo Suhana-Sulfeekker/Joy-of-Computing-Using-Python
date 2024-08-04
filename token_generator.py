83fa8dffca46f9b4a1f91cc637ee4a075ab98648
Suhana-Sulfeekker <121898430+Suhana-Sulfeekker@users.noreply.github.com>
2024-08-05 01:38:25 +0530

    while loop

---
+++ token_generator.py
@@ -0,0 +1,8 @@
+print("Hello everyone, we are starting.")
+n=1
+c=1
+while(c==1):
+  print("token number",n,"may please come in")
+  c=int(input("Continue?(0/1)"))
+  n=n+1
+print("Thank you, this is the end of our day.")
